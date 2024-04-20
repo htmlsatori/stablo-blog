52f3ab7adeb7c0ccce0d4d1469b23bbac480a1f9
htmlsatori <satorijal@gmail.com>
2024-04-20 14:10:34 -0400

    function removed

--- components/ui/label.js
+++ components/ui/label.js
@@ -28,14 +28,5 @@ export default function Label(props) {
     );
   }
 
-  return (
-    <span
-      className={cx(
-        "inline-block text-xs font-medium tracking-wider uppercase ",
-        !margin && " mt-5",
-        color[props.color] || color[pink]
-      )}>
-      {props.children}
-    </span>
-  );
+
 }
