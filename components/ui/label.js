80706da308fce67425fa25327192cfe67abf8eb4
htmlsatori <satorijal@gmail.com>
2024-04-20 14:02:59 -0400

    category text blue to black

--- components/ui/label.js
+++ components/ui/label.js
@@ -3,7 +3,7 @@ import { cx } from "@/utils/all";
 export default function Label(props) {
   const color = {
     green: "text-emerald-700",
-    blue: "text-blue-600",
+    blue: "text-black",
     orange: "text-orange-700",
     purple: "text-purple-600",
     pink: "text-pink-600"
